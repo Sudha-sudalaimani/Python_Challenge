4642029f6e90a9c0c041eb33b3b883d763deaf51
TheDevJournal <s.sudha19082006@gmail.com>
2025-10-27 13:04:25 +0530

    Create LeapYear.py

---
+++ LeapYear.py
@@ -0,0 +1,9 @@
+year=int(input("Enter Year:"))
+if year%4==0:
+    if year %100==0:
+        if year%400==0:
+            print("Leap Year")
+    else:
+        print("Leap Year")
+else:
+    print("Not a Leap Year")
