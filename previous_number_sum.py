e9bafe01edad1e47c72a9964a5adb4700f013d73
TheDevJournal <s.sudha19082006@gmail.com>
2025-12-06 08:51:22 +0530

    Create previous_number_sum.py

---
+++ previous_number_sum.py
@@ -0,0 +1,6 @@
+def previous_number_sum():
+    print("Printing Current and previous Number sum in range(10)")
+    for i in range(10):
+        total=i+i-1
+        print(f"Current Number {i} Previous Number {i-1} sum : {total}")
+previous_number_sum()
